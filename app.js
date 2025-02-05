8e6bd3e73a521d9b23b9e74ed376ca8fa3565405
alejjandro <luistosne085@gmail.com>
2025-02-05 12:44:08 -0500

    se agrego una funcion para actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //1. crear un array para almacenar los nombres
-let amigos =[];
+let amigos = [];
 
 //2. Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
 //Capturar la info
 function agregarAmigo() {
-    const nombreAmigo = document.getElementById('amigo').value;
-    
-    //Validar
-    if(nombreAmigo.trim()=== ''){
-        alert('Por favor, incerte un nombre');
-        return;
-    }
-    //Actulizar el array
-    amigos.push(nombreAmigo);
-    //Limpiar campo
-    document.getElementById('amigo').value='';
+  const nombreAmigo = document.getElementById("amigo").value;
+
+  //Validar
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, incerte un nombre");
+    return;
+  }
+  //Actulizar el array
+  amigos.push(nombreAmigo);
+  //Limpiar campo
+  document.getElementById("amigo").value = "";
+  //Actualizar lista
+  actualizarLista();
 }
-    document.getElementById('button-add').addEventListener('click',agregarAmigo);
+//Funcion para actualizar la lista
+function actualizarLista() {
+  const listaAmigos = document.getElementById("listaAmigos");
+  //Limpiar la lista
+  listaAmigos.innerHTML = "";
+
+  //Iterar sombre el arreglo
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    listaAmigos.appendChild(li);
+  }
+}
+document.getElementById("button-add").addEventListener("click", agregarAmigo);
